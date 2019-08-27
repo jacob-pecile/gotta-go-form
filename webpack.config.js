6edc455dba846e8fe6cfe3ea0c63677aef935fe8
Jacob Pecile <jacob.pecile@gmail.com>
2019-08-27 15:50:29 -0400

    configuration and hello world

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+"use strict";
+
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const extractSass = new MiniCssExtractPlugin({
+    filename: "./dist/[name].css",   
+});
+
+var DefinePlugin = require("webpack").DefinePlugin;
+const path = require('path');
+
+module.exports = env => {
+
+    const envConfig = Object.keys(env)[0] === 'dev' ? new DefinePlugin({
+        'process.env': {
+            'NODE_ENV': JSON.stringify('development')
+        }
+    }) : new DefinePlugin({
+        'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+        }
+    });
+
+    return {
+        entry:{
+            "GottaGoForm":["babel-polyfill", "./App.tsx"]
+        },
+        output: {
+            path:  path.resolve(__dirname, "."),
+            filename: "./dist/[name].js",
+            libraryTarget: 'umd',
+            umdNamedDefine: true,
+            library: "./lib"
+        },
+        module: {
+            rules: [
+                { 
+                    test: /\.tsx?$/,
+                    loader: "babel-loader!ts-loader"
+                },                        
+                {
+                    test: /\.jsx?$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    options: {
+                        presets: ['react', 'es2015']
+                    }
+                },   
+                { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+                {
+                    test: /.(\.scss|\.css)$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                }
+            ]
+        },
+        devtool: "sourcemap",
+        plugins:[
+            extractSass,
+            envConfig
+        ],
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".json", ".css"]
+        },
+        devServer:{
+            contentBase: "./",
+            port: 7777
+        }
+    }
+}
